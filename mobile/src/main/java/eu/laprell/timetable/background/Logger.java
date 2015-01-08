8a0481de23b48d85048e74f34e63545a8b3a449a
David Laprell <david@laprell.eu>
2015-01-08 21:29:48 +0100

    Future: Logger utils to save the log messages

---
+++ mobile/src/main/java/eu/laprell/timetable/background/Logger.java
@@ -0,0 +1,12 @@
+package eu.laprell.timetable.background;
+
+/**
+ * Created by david on 08.01.15.
+ */
+public class Logger {
+
+    /* TODO: Use this method and class to log to data and allow to upload it */
+    public static void log(String tag, String msg, Throwable t) {
+
+    }
+}
